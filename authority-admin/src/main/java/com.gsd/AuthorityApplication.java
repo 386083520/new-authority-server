b7fa8a2c77ffbffa8241622acf4ca78196e5f436
386083520 <386083520@qq.com>
2022-09-03 16:37:15 +0800

    配置数据库连接

--- authority-admin/src/main/java/com.gsd/AuthorityApplication.java
+++ authority-admin/src/main/java/com.gsd/AuthorityApplication.java
@@ -2,8 +2,9 @@ package com.gsd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class AuthorityApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthorityApplication.class, args);
