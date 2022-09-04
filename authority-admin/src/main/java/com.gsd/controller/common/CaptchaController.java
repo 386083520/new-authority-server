5a5675471f447ec98836578a932ced1fc085c73f
386083520 <386083520@qq.com>
2022-09-04 13:52:20 +0800

    验证码转化为图片并返回

--- authority-admin/src/main/java/com.gsd/controller/common/CaptchaController.java
+++ authority-admin/src/main/java/com.gsd/controller/common/CaptchaController.java
@@ -3,11 +3,16 @@ package com.gsd.controller.common;
 import com.google.code.kaptcha.Producer;
 import com.gsd.common.core.domain.AjaxResult;
 import com.gsd.common.utils.uuid.IdUtils;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FastByteArrayOutputStream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 @RestController
 public class CaptchaController {
@@ -24,12 +29,21 @@ public class CaptchaController {
         }
         String uuid = IdUtils.simpleUUID();
         String captchaType = "math";
+        BufferedImage image = null;
         if("math".equals(captchaType)) {
             String capText = captchaProducerMath.createText();
             System.out.println(capText);
+            image = captchaProducerMath.createImage(capText);
         }
+        FastByteArrayOutputStream os = new FastByteArrayOutputStream();
+        try {
+            ImageIO.write(image, "jpg", os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         ajax.put("uuid", uuid);
-        ajax.put("img", "abc");
+        ajax.put("img", Base64.encode(os.toByteArray()));
         return ajax;
     }
 }
