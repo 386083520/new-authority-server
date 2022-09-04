9da6b5e7a1ac70556f47c75b1840b513e671b418
386083520 <386083520@qq.com>
2022-09-04 13:31:34 +0800

    kaptcha引入

--- authority-admin/src/main/java/com.gsd/controller/common/CaptchaController.java
+++ authority-admin/src/main/java/com.gsd/controller/common/CaptchaController.java
@@ -1,12 +1,20 @@
 package com.gsd.controller.common;
 
+import com.google.code.kaptcha.Producer;
 import com.gsd.common.core.domain.AjaxResult;
 import com.gsd.common.utils.uuid.IdUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class CaptchaController {
+    @Resource(name = "captchaProducerMath")
+    private Producer captchaProducerMath;
+
+
     @GetMapping("/captchaImage")
     public AjaxResult getCode() {
         AjaxResult ajax = AjaxResult.success();
@@ -15,6 +23,11 @@ public class CaptchaController {
             return ajax;
         }
         String uuid = IdUtils.simpleUUID();
+        String captchaType = "math";
+        if("math".equals(captchaType)) {
+            String capText = captchaProducerMath.createText();
+            System.out.println(capText);
+        }
         ajax.put("uuid", uuid);
         ajax.put("img", "abc");
         return ajax;
---
+++ authority-framework/src/main/java/com/gsd/framework/config/CaptchaConfig.java
@@ -0,0 +1,26 @@
+package com.gsd.framework.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class CaptchaConfig {
+    @Bean(name = "captchaProducer")
+    public DefaultKaptcha getKaptchaBean() {
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        return defaultKaptcha;
+    }
+
+    @Bean(name = "captchaProducerMath")
+    public DefaultKaptcha getKaptchaBeanMath() {
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        Properties properties = new Properties();
+        Config config = new Config(properties);
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+}
