f8804c2fdf6dc6a0839585405c05567daf09530a
386083520 <386083520@qq.com>
2022-11-07 10:57:38 +0800

    从数据库查询权限

--- authority-framework/src/main/java/com/gsd/framework/web/service/SysPermissionService.java
+++ authority-framework/src/main/java/com/gsd/framework/web/service/SysPermissionService.java
@@ -1,6 +1,8 @@
 package com.gsd.framework.web.service;
 
 import com.gsd.common.core.domain.entity.SysUser;
+import com.gsd.system.service.ISysMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
@@ -8,12 +10,15 @@ import java.util.Set;
 
 @Component
 public class SysPermissionService {
+    @Autowired
+    private ISysMenuService menuService;
+
     public Set<String> getMenuPermission(SysUser user) {
         Set<String> perms = new HashSet<String>();
         if(user.isAdmin()) {
             perms.add("*:*:*");
         }else {
-
+            perms.addAll(menuService.selectMenuPermsByUserId(user.getUserId()));
         }
         return perms;
     }
--- authority-system/src/main/java/com/gsd/system/service/impl/SysMenuServiceImpl.java
+++ authority-system/src/main/java/com/gsd/system/service/impl/SysMenuServiceImpl.java
@@ -1,13 +1,29 @@
 package com.gsd.system.service.impl;
 
+import com.gsd.common.utils.StringUtils;
+import com.gsd.system.mapper.SysMenuMapper;
 import com.gsd.system.service.ISysMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class SysMenuServiceImpl implements ISysMenuService{
+    @Autowired
+    private SysMenuMapper menuMapper;
+
     public Set<String> selectMenuPermsByUserId(Long userId) {
-        return null;
+        List<String> perms = menuMapper.selectMenuPermsByUserId(userId);
+        Set<String> permsSet = new HashSet<String>();
+        for (String perm: perms) {
+            if(StringUtils.isNotEmpty(perm)) {
+                permsSet.addAll(Arrays.asList(perm.trim().split(",")));
+            }
+        }
+        return permsSet;
     }
 }
